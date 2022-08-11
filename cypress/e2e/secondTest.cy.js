46c1152f969f04f0fab6a6a8e1cc6868e5a94962
Anas <anasansarimuhammad@gmail.com>
2022-08-11 17:47:53 +0500

    Docker image

--- cypress/e2e/secondTest.cy.js
+++ cypress/e2e/secondTest.cy.js
@@ -7,7 +7,7 @@ beforeEach('login to the app', () =>{
 it("Verify user can logout sucessfully", () => {
     cy.contains('Settings').click()
     cy.contains(' Or click here to logout. ').click()
-    cy.get('.navbar-nav').should('contain',' Sign up')
+    cy.get('.navbar-nav').should('contain','Sign up')
 
 })
 
